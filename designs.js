7f514dcaf36e337622f47c2e1a9dae7b45b2690e
Sophia402 <imafidonroselyn7@gmail.com>
2022-06-08 09:08:50 -0600

    java script code

--- designs.js
+++ designs.js
@@ -1,10 +1,38 @@
 // Select color input
-// Select size input
+// Select size 
+
 
 // When size is submitted by the user, call makeGrid()
 
-function makeGrid() {
+    
+    function makeGrid(height, width){
+    
+    	for(let i=0; i<= height; i++){
+    		let row = table.insertRow(i);
+    		for(let r=0; r<= width; r++){
+    			let cell = row.insertCell(j);
+    			cell.addEventListener("click", (o) => {
+    				cell.style.backgroundColor = color.value;
+    			})
+    		}
+    	}
+    }
 
 // Your code goes here!
-
-}
+let color = document.getElementById("colorPicker");
+    let table = document.getElementById("pixelCanvas");
+    let sizePicker = document.getElementById("sizePicker");
+    
+    let height = document.getElementById("inputHeight").value;
+    let width = document.getElementById("inputWeight").value;
+    makeGrid(height, width);
+    
+    sizePicker.addEventListener ("click", (o) => {
+    
+      o.preventDefault();
+    
+      let height = document.getElementById("inputHeight").value;
+      let width = document.getElementById("inputWeight").value;
+      table.firstElementChild.remove();     
+    }
+    )
