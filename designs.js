6eaf56c3c2a1af2d421b38282b0df11735dab30a
Sophia402 <imafidonroselyn7@gmail.com>
2022-06-24 13:05:13 -0600

    pixel art working good

--- designs.js
+++ designs.js
@@ -4,42 +4,49 @@
 
 // When size is submitted by the user, call makeGrid()
 
+  
+  
+  // Your code goes here!
+  let color = document.getElementById("colorPicker");
+  let table = document.getElementById("pixelCanvas");
+  let sizePicker = document.getElementById("sizePicker");
+  
+  let height = document.getElementById("inputHeight").value;
+  let width = document.getElementById("inputWidth").value;
+  
+  
 
-function makeGrid(height, width) {
+  // Select color input
+  // Select size 
+  
+  
+  // When size is submitted by the user, call makeGrid()
+  
+  
+  function makeGrid(height, width) {
+    table.innerHTML = '';
 
-  for (let i = 0; i <= height; i++) {
-    let row = table.insertRow(i);
-    for (let r = 0; r <= width; r++) {
-      let cell = row.insertCell(r);
-      cell.addEventListener("click", (o) => {
-        cell.style.backgroundColor = color.value;
-      })
+    for (let i = 0; i < height; i++) {
+      let row = table.insertRow(i);
+      for (let r = 0; r < width; r++) {
+        let cell = row.insertCell(r);
+        cell.addEventListener("click", (o) => {
+          cell.style.backgroundColor = color.value;
+        })
+      }
     }
   }
-}
-
-// Your code goes here!
-let color = document.getElementById("colorPicker");
-let table = document.getElementById("pixelCanvas");
-let sizePicker = document.getElementById("sizePicker");
-
-let height = document.getElementById("inputHeight").value;
-let width = document.getElementById("inputWidth").value;
-makeGrid(height, width);
-
-sizePicker.addEventListener("click", (o) => {
-
-  o.preventDefault();
-
-  let height = document.getElementById("inputHeight").value;
-  let width = document.getElementById("inputWeight").value;
-  table.firstElementChild.remove();
-}
-)
-
-const form = document.getElementsById("sizePicker");
-form.addEventListener("submit", respondToClick);
-
-function respondToClick(event) {
-  alert("clicked")
-}
+  
+  
+  
+  
+  sizePicker.addEventListener("submit", (o) => {
+  
+    o.preventDefault();
+  
+    let height = document.getElementById("inputHeight").value;
+    let width = document.getElementById("inputWidth").value;
+    makeGrid(height, width);
+  }
+  )
+  
