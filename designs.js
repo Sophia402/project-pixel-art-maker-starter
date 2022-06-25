3b8b74aef7728937b405cdd936e24be9e190aeef
Sophia402 <imafidonroselyn7@gmail.com>
2022-06-25 09:39:48 -0600

    comments

--- designs.js
+++ designs.js
@@ -10,7 +10,7 @@
   let color = document.getElementById("colorPicker");
   let table = document.getElementById("pixelCanvas");
   let sizePicker = document.getElementById("sizePicker");
-  
+  //selecting grid meaurement
   let height = document.getElementById("inputHeight").value;
   let width = document.getElementById("inputWidth").value;
   
@@ -25,11 +25,12 @@
   
   function makeGrid(height, width) {
     table.innerHTML = '';
-
+  //creating row
     for (let i = 0; i < height; i++) {
       let row = table.insertRow(i);
       for (let r = 0; r < width; r++) {
         let cell = row.insertCell(r);
+        //creating event listener
         cell.addEventListener("click", (o) => {
           cell.style.backgroundColor = color.value;
         })
@@ -39,7 +40,7 @@
   
   
   
-  
+  // making grids
   sizePicker.addEventListener("submit", (o) => {
   
     o.preventDefault();
